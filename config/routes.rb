0947c4919eb2b88d623e2ab8a3f3958271fe1ff1
Chaewan Shin <chaewanshin@gmail.com>
2024-08-06 19:34:54 +0900

    file updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Read
+  get 'articles', to: 'articles#index', as: :articles
+  # Create an article
+  # need page for a form
+  get 'article/new', to: 'articles#new', as: :new_article
+  # a place to send form and create instance
+  post 'articles', to: 'articles#create'
+  # read one task, prefix here to be singular
+  get 'articles/:id', to: 'articles#show', as: :article
+  # update/edit
+  # page for a form
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  # a plce to send the form info and update the instance
+  patch 'articles/:id', to: 'articles#update'
+  # delete
+  delete 'articles/:id', to: 'articles#destroy'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Books::CultureSeries.book, content: Faker::Books::CultureSeries.culture_ship)
+end
